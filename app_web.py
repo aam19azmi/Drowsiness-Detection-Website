6af0bcf0f83708cad76b7dc20670e56801caf6a6
Azmi Jalaluddin Amron <azmijalaluddin@gmail.com>
2026-05-11 02:53:14 +0700

    update OpenRelay

--- app_web.py
+++ app_web.py
@@ -55,9 +55,14 @@ RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [
         {"urls": ["stun:stun.l.google.com:19302"]},
         {"urls": ["stun:stun1.l.google.com:19302"]},
-        {"urls": ["stun:stun2.l.google.com:19302"]},
-        {"urls": ["stun:stun3.l.google.com:19302"]},
-        {"urls": ["stun:stun4.l.google.com:19302"]},
+        {
+            "urls": [
+                "turn:openrelay.metered.ca:80",
+                "turn:openrelay.metered.ca:443"
+            ],
+            "username": "openrelayproject",
+            "credential": "openrelayproject"
+        }
     ]}
 )
 
